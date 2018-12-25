a3c5a0d67120d317d388596e828f7846c3a66751
elija <elijah.b.i.p@gmail.com>
2018-12-26 02:04:09 +0400

    Добавьте файлы проекта.

---
+++ third_l/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace third_l
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
